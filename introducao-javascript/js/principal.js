9b7f8eb81c0e592d32c559efc0d00fcd258ed013
JordanBoaventuraRibas <36683470+JordanBoaventuraRibas@users.noreply.github.com>
2019-01-30 21:54:26 -0200

    etc.
    etc.

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -7,26 +7,29 @@ titulo.textContent = "Aparecida Nutricionista";
 var pacientes = document.querySelectorAll(".paciente");
 
 for(i = 0 ; i < pacientes.length ; i++){
-    var tdpeso = pacientes[i].querySelector(".info-peso");
+    var paciente = pacientes[i];
+    var tdpeso = paciente.querySelector(".info-peso");
     var peso = tdpeso.textContent;
 
-    var tdaltura = pacientes[i].querySelector(".info-altura");
+    var tdaltura = paciente.querySelector(".info-altura");
     var altura = tdaltura.textContent;
 
-    var tdimc = pacientes[i].querySelector(".info-imc");
+    var tdimc = paciente.querySelector(".info-imc");
 
     var veraltura = true;
     var verpeso = true;
 
     if(peso <= 0 || peso >= 500){
         tdimc.textContent = "Peso inválido!";
+        paciente.classList.add("paciente-invalido");
         verpeso = false;
     }
     if(altura <=0 || altura >= 3){
         tdimc.textContent = "Altura inválida!";
+        paciente.classList.add("paciente-invalido");
         veraltura = false;
     }
-
+    
     if(veraltura && verpeso){
         var imc = peso / (altura * altura);
         tdimc.textContent = imc.toFixed(2);
